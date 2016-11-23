7b152f9cdbe6c5687a74010c9a878915e02f9d6b
Joao Miranda <jvdmmiranda@gmail.com>
2016-11-23 10:33:56 -0200

    adicionando tabela typechart

--- create.sql
+++ create.sql
@@ -68,3 +68,27 @@ create table pokemons (
     primary key(id)
 );
 
+create table typechart (
+    id serial not null,
+    defensetype1 varchar(100), 
+    defensetype2 varchar(100),
+    normal varchar(10),
+    fire varchar(10),
+    water varchar(10),
+    electric varchar(10),
+    grass varchar(10),
+    ice varchar(10),
+    fighting varchar(10),
+    poison varchar(10),
+    ground varchar(10),
+    flying varchar(10),
+    psychic varchar(10),
+    bug varchar(10),
+    rock varchar(10),
+    ghost varchar(10),
+    dragon varchar(10),
+    dark varchar(10),
+    steel varchar(10),
+    fairy varchar(10),
+    primary key(id)
+);
